f58b0446427f418d9793aa5c1e55502b204fc330
ethan <manchon.ethan@gmail.com>
2025-04-28 09:55:31 +0200

    Sauvegarde des modifications avant de passer à la branche build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   css: ['/assets/css/main.css'],
+  
+  app: {
+    baseURL: '/Multi-step-form/', 
+  },
+
+  ssr: false, 
 })
+
