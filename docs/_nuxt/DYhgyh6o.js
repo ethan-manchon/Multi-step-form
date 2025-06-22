32b870b3cd9e5c9c81825debca4c654b6a6858a2
Ethan <ethan.manchon@etu.unilim.fr>
2025-06-22 18:04:17 +0200

    git pages

--- docs/_nuxt/DYhgyh6o.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./DvGW6bTD.js";const s={__name:"index",setup(r){return e().replace("/step1"),()=>{}}};export{s as default};
---
+++ docs/_nuxt/DnoZQRtJ.js
@@ -0,0 +1,1 @@
+import{e}from"./CFPSwLoW.js";const s={__name:"index",setup(r){return e().replace("/step1"),()=>{}}};export{s as default};
